bba9f8aa321120c6a2eb36f8e87d2996c0a43a01
Анастасия Киселева <nastena.kiseleva.05@bk.ru>
2025-12-01 00:10:39 +0400

    Task 5

--- scripts/like.js
+++ scripts/like.js
@@ -2,23 +2,28 @@
 ✦ like-icon — для svg-иконки анимированного сердца
 ✦ card__like-button — для кнопки Like рядом с иконкой
 ✦ card__icon-button — для кнопки, оборачивающей иконку
-✦ card__icon-button — для кнопки, оборачивающей иконку
 ✦ is-liked — для обозначения состояния лайкнутой иконки в виде сердца
 ✦ button__text — для обозначения текстового элемента внутри кнопки
-Если эти классы поменять в HTML, скрипт перестанет работать. Будьте аккуратны.
 */
 
 const likeHeartArray = document.querySelectorAll('.like-icon');
 const likeButtonArray = document.querySelectorAll('.card__like-button');
 const iconButtonArray = document.querySelectorAll('.card__icon-button');
 
+// Обработчик для кнопки с иконкой
 iconButtonArray.forEach((iconButton, index) => {
-  iconButton.onclick = () =>
+  iconButton.addEventListener('click', function(event) {
+    event.preventDefault(); // Предотвращаем перезагрузку
     toggleIsLiked(likeHeartArray[index], likeButtonArray[index]);
+  });
 });
 
+// Обработчик для текстовой кнопки Like
 likeButtonArray.forEach((button, index) => {
-  button.onclick = () => toggleIsLiked(likeHeartArray[index], button);
+  button.addEventListener('click', function(event) {
+    event.preventDefault(); // Предотвращаем перезагрузку
+    toggleIsLiked(likeHeartArray[index], button);
+  });
 });
 
 function toggleIsLiked(heart, button) {
@@ -27,7 +32,7 @@ function toggleIsLiked(heart, button) {
 }
 
 function setButtonText(heart, button) {
-  if ([...heart.classList].includes('is-liked')) {
+  if (heart.classList.contains('is-liked')) {
     setTimeout(
       () => (button.querySelector('.button__text').textContent = 'Unlike'),
       500
